27fcb187bb7824253b77bf90338bb22f0080920b
Agus Riyanto <agsriyanto@M-W50HHC2M77-2.local>
2023-06-07 22:56:36 +0700

    added navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav class="bg-[#292e35] flex items-center gap-11 justify-center max-h-16">
+    <div class="cursor-pointer">
+      <img src="../static/MoovieTime-Logo.svg" alt="logo-1" />
+    </div>
+    <div class="flex items-center gap-4 bg-[#1e232a] p-2 px-4 rounded">
+      <img src="../static/icon-mov.svg" alt="logo-2" />
+      <input type="text" placeholder="Find movie" class="bg-[#1e232a] w-96" />
+      <img src="../static/icon-search.svg" alt="logo-search" class="w-4" />
+    </div>
+    <div>
+      <ul class="flex gap-11">
+        <li>
+          <div class="flex gap-4 cursor-pointer" id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar">
+            <img src="../static/icon-category.svg" alt="logo-category" class="w-5" />
+            <a class="uppercase text-white">Categories</a>
+          </div>
+          <div id="dropdownNavbar" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+            <ul class="py-2 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdownLargeButton">
+              <li>
+                <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Action</a>
+              </li>
+              <li>
+                <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Adventure</a>
+              </li>
+              <li>
+                <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Animation</a>
+              </li>
+            </ul>
+        </div>
+        </li>
+        <li>
+          <a class="uppercase text-white cursor-pointer">Movies</a>
+        </li>
+        <li>
+          <a class="uppercase text-white cursor-pointer">TV Shows</a>
+        </li>
+        <li>
+          <a class="uppercase text-white cursor-pointer">LOGIN</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <section class="container bg-[#1e232a]">
+    <Navbar class="fixed top-0 p-4 w-full" />
+    <div>
+      <app-logo/>
+      <h1 class="title">
+        movie-perqara
+      </h1>
+      <h2 class="subtitle">
+        Movie with Nuxt
+      </h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green">Documentation</a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey">GitHub</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+import Navbar from '../components/Navbar.vue';
+
+export default {
+  components: {
+    AppLogo,
+    Navbar,
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  min-width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
+
